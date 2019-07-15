786d58ff9447b974fe2a3c77c30b00252439684c
noideawut2put <43249742+noideawut2put@users.noreply.github.com>
2019-07-15 19:22:14 -0400

    initial commit

---
+++ nflcompare/app.py
@@ -0,0 +1,24 @@
+
+from flask import Flask, render_template, redirect
+from flask import request as flask_request
+
+def create_app():
+    """Create and configure an instance of the Flask application."""
+
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        """Homepage."""
+        return render_template('base.html', title='Home')
+
+    @app.route('/compare_pick', methods=['POST'])
+    def compare_two_players():
+        """
+        Baseline Season Long Pick.
+        """
+        player1, player2 = flask_request.values['player1'], flask_request.values['player2']
+
+    
+    
+    return app
---
+++ nflcompare/nflreferenceScrape.py
@@ -0,0 +1,1 @@
+""" This will have all of the functions that scrape from pro-football-reference.com """
