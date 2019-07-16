b8e1a69fb6451a0063ac70fea322d2e5b0c900e5
noideawut2put <43249742+noideawut2put@users.noreply.github.com>
2019-07-15 21:08:30 -0400

    added scraping functions

--- nflcompare/app.py
+++ nflcompare/app.py
@@ -6,11 +6,11 @@ def create_app():
     """Create and configure an instance of the Flask application."""
 
     app = Flask(__name__)
-
+    positions = ['QB', 'RB', 'WR', 'TE', 'K', 'DEF']
     @app.route('/')
     def home():
         """Homepage."""
-        return render_template('base.html', title='Home')
+        return render_template('base.html', title='Home', positions=positions)
 
     @app.route('/compare_pick', methods=['POST'])
     def compare_two_players():
@@ -19,6 +19,6 @@ def create_app():
         """
         player1, player2 = flask_request.values['player1'], flask_request.values['player2']
 
-    
-    
+        return render_template('base.html', title='Home', selected_position=None)
+
     return app
